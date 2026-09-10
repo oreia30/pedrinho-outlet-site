0271c79f89ac840de90d596c8491ca493c7a5e26
oreia30 <68307186+oreia30@users.noreply.github.com>
2026-09-10 11:37:34 -0300

    add api/index.js

---
+++ api/index.js
@@ -0,0 +1,3 @@
+// Ponto de entrada pra rodar como Serverless Function na Vercel.
+// Zero-config: qualquer arquivo dentro de /api vira uma função automaticamente.
+module.exports = require('../server.js');
